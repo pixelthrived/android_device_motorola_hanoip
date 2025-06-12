0fceb09c995bc60254ea3962a1011771aabbba4f
PixelThrived <91941157+pixelthrived@users.noreply.github.com>
2025-06-12 08:16:35 +0300

    hanoip: introduce device detection for camera prop settings
    Change-Id: I70c1db3844bdffc457b4eec4b3ffc1cbed1b439c
    Signed-off-by: PixelThrived <91941157+pixelthrived@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -518,6 +518,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Camera props (device detection)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.hanoip.axion.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.hanoip.axion.rc
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
